8d6ad3dc1512758f5eec720864a19b0cef2993a4
RahulNeupane <nrahul503@gmail.com>
2022-09-10 21:06:29 +0545

    pokedex

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const poke_container = document.getElementById('poke-container')
-const poke_count = 150
+const pokeContainer = document.getElementById('poke-container')
+const pokeCount =  150
+
 const colors = {
     fire: '#fddfdf',
     grass: '#DEFDE0',
@@ -17,48 +18,46 @@ const colors = {
 	normal: '#F5F5F5'
 }
 
-const main_types = Object.keys(colors )
+const main_types = Object.keys(colors)
 
-const fetchPokemons = async ()=>  {
-    for(let i = 1; i<= poke_count; i++){
+const fetchPokemons = async ()=>{
+    for(let i = 1; i<= pokeCount; i++){
         await getPokemon(i)
     }
 }
 
-const getPokemon = async (id) => {
+const getPokemon = async (id) =>{
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const res = await fetch(url)
     const data = await res.json()
     createPokemonCard(data)
 }
 
-const createPokemonCard = (pokemon)=>{
+const createPokemonCard = (pokemon) =>{
     const pokeEl = document.createElement('div')
     pokeEl.classList.add('pokemon')
-    const name =  pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
 
-    const id = pokemon.id.toString().padStart(3, '0')
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3,'0')
 
     const poke_types = pokemon.types.map(type => type.type.name)
-
     const type = main_types.find(type => poke_types.indexOf(type) > -1)
-
+    
     const color = colors[type]
     pokeEl.style.backgroundColor = color
-    
-    const pokemonInnerHTML = ` 
+    const pokemonInnerHTML = `
         <div class="img-container">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="pokemon">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="">
         </div>
         <div class="info">
-          <span class="number">#${id}</span>
-          <h3 class="name">${name}</h3>
-          <small class="type">Type: <span>${type}</span></small>
-        </div>  
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span></small>
+        </div>
     `
 
     pokeEl.innerHTML = pokemonInnerHTML
-    poke_container.appendChild(pokeEl)
+    pokeContainer.appendChild(pokeEl)
 }
 
 fetchPokemons()
